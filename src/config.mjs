ec5a7d9db5e18ef71dede6df892910104aa3ba51
Tomaso Ethan <62837924+Neonexus45@users.noreply.github.com>
2025-02-04 16:45:21 +0100

    hiding config

--- src/config.mjs
+++ src/config.mjs
@@ -2,11 +2,11 @@ export default {
   development: {
     type: 'development',
     port: 3000,
-    mongodb: 'mongodb+srv://ethantomaso:c2GEeTqVVeWRBSx8@efrei.cw9n1.mongodb.net/api'
+    mongodb: 'A REMPLIR'
   },
   production: {
     type: 'production',
     port: 3000,
-    mongodb: 'mongodb+srv://ethantomaso:c2GEeTqVVeWRBSx8@efrei.cw9n1.mongodb.net/api'
+    mongodb: 'A REMPLIR'
   }
 };
